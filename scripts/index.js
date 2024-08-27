df7146e8678209afd4491ab8012f8bc67c0d8b73
Vsevolod Scherbinin <vsevolod.o.sh@yandex.ru>
2024-08-27 15:13:59 +0300

    TMA_Game_1_OOP - v.0.3.3 Invitation Test

--- scripts/index.js
+++ scripts/index.js
@@ -233,5 +233,36 @@ window.addEventListener('beforeunload', (evt) => {
 });
 // --------------- Window-End ---------------
 
+// Инициализация Telegram Web App
+const tg = window.Telegram.WebApp;
+
+// Функция для обработки параметров при запуске бота
+function handleStartParams() {
+    const params = tg.initDataUnsafe;
+
+    // Проверяем, есть ли параметры
+    if (params && params.query) {
+        const queryParams = new URLSearchParams(params.query);
+        const referralId = queryParams.get('referral_id');
+
+        if (referralId) {
+            // Отправляем приветственное сообщение с информацией о приглашении
+            alert(`Вы пришли по приглашению пользователя с ID: ${referralId}`);
+        }
+    }
+}
+
+// Обработчик нажатия на кнопку
+document.querySelector('.tasksScreen__inviteBtn').onclick = function() {
+    const inviteLink = `https://t.me/${tg.initDataUnsafe.user.username}?start=invite_friends&referral_id=${tg.initDataUnsafe.user.id}`;
+    const message = `Пригласите своих друзей по ссылке: ${inviteLink}`;
+
+    // Отправляем сообщение в чат с помощью Telegram Web App API
+    tg.sendMessage(message);
+};
+
+// Вызываем функцию для обработки параметров
+handleStartParams();
+
 // nameField.textContent = TMA.initDataUnsafe.user.first_name;
 // console.log(TMA);
