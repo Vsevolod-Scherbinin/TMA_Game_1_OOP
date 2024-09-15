126b8573a47575e71b72e2800ec520aeea9a164d
Vsevolod Scherbinin <vsevolod.o.sh@yandex.ru>
2024-09-15 13:55:49 +0300

    TMA_Game_1_OOP - 0.11.5.1 Tasks - Level Statuses

---
+++ utils/levelsStatuses.js
@@ -0,0 +1,58 @@
+const levelsStatuses = [
+  {
+    level: 1,
+    status: 'Новичок',
+  },
+  {
+    level: 10,
+    status: 'Начинающий',
+  },
+  {
+    level: 20,
+    status: 'Любитель',
+  },
+  {
+    level: 30,
+    status: 'Игрок',
+  },
+  {
+    level: 40,
+    status: 'Спец',
+  },
+  {
+    level: 50,
+    status: 'Профи',
+  },
+  {
+    level: 60,
+    status: 'Эксперт',
+  },
+  {
+    level: 70,
+    status: 'Мастер',
+  },
+  {
+    level: 80,
+    status: 'Гуру',
+  },
+  {
+    level: 90,
+    status: 'Ac',
+  },
+  {
+    level: 100,
+    status: 'Рокфеллер',
+  },
+  {
+    level: 110,
+    status: 'Магнат',
+  },
+  {
+    level: 120,
+    status: 'Палец ярости',
+  },
+  {
+    level: 130,
+    status: 'Легенда',
+  },
+];
