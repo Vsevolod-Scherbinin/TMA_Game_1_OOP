2cf34f9cb9c502495a0a11f6572a9a37878380f6
Vsevolod Scherbinin <vsevolod.o.sh@yandex.ru>
2024-09-16 00:02:02 +0300

    TMA_Game_1_OOP - 0.13.4 Tasks Conditions Friends

--- utils/dailyTasks.js
+++ utils/dailyTasks.js
@@ -25,4 +25,30 @@ const dailyTasks = [
       },
     ]
   },
+  {
+    date: '16.09.2024',
+    tasks: [
+      {
+        id: 1,
+        type: 'friend',
+        mainIcon: 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7',
+        // mainIcon: './images/done.png',
+        title: "Пригласить друга",
+        description: 'Пригласите в игру 1 друга',
+        effectIcon: "./images/coin.png",
+        effect: 10000,
+      },
+      {
+        id: 2,
+        type: 'channel',
+        mainIcon: './images/done.png',
+        channelId: -1002493343663,
+        channelLink: 'https://t.me/+cU6JKcOAFuphZTli',
+        title: "Подписаться на канал",
+        description: `Название`,
+        effectIcon: "./images/coin.png",
+        effect: 10000,
+      },
+    ]
+  },
 ]
