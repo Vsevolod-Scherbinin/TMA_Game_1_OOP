356fedb3c421e3fcb7133e9abff72257266e4072
Vsevolod Scherbinin <vsevolod.o.sh@yandex.ru>
2024-09-15 21:38:24 +0300

    TMA_Game_1_OOP - 0.11.7.10 Tasks Cards Style LS Clear

--- scripts/index.js
+++ scripts/index.js
@@ -102,8 +102,8 @@ try {
   if(tg.initDataUnsafe.user.first_name.length>0) {
     console.log('tgData', tg.initDataUnsafe);
     nameField.textContent = tg.initDataUnsafe.user.first_name;
-    const userPhoto = tg.initDataUnsafe.user.photo_url;
-    avatarField.src = userPhoto;
+    // const userPhoto = tg.initDataUnsafe.user.photo_url;
+    // avatarField.src = userPhoto;
   }
 } catch {}
 
