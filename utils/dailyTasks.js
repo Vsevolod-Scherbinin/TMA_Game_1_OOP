2f2be9d758df0e4d2436f1f6ca7e2424f97d5fc5
Vsevolod Scherbinin <vsevolod.o.sh@yandex.ru>
2024-09-21 10:03:34 +0300

    TMA_Game_1_OOP - 0.16 Layout New Icons

--- utils/dailyTasks.js
+++ utils/dailyTasks.js
@@ -155,4 +155,82 @@ const dailyTasks = [
       },
     ]
   },
+  {
+    date: '21.09.2024',
+    tasks: [
+      {
+        id: 1,
+        type: 'friend',
+        mainIcon: 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7',
+        // mainIcon: './images/done.png',
+        title: "Пригласить друга",
+        description: 'Пригласите в игру 1 друга',
+        effectIcon: "./images/coin.png",
+        effect: 10000,
+      },
+      {
+        id: 2,
+        type: 'channel',
+        mainIcon: './images/done.png',
+        channelId: -1002493343663,
+        channelLink: 'https://t.me/+cU6JKcOAFuphZTli',
+        title: "Подписаться на канал",
+        description: `Название`,
+        effectIcon: "./images/coin.png",
+        effect: 10000,
+      },
+    ]
+  },
+  {
+    date: '22.09.2024',
+    tasks: [
+      {
+        id: 1,
+        type: 'friend',
+        mainIcon: 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7',
+        // mainIcon: './images/done.png',
+        title: "Пригласить друга",
+        description: 'Пригласите в игру 1 друга',
+        effectIcon: "./images/coin.png",
+        effect: 10000,
+      },
+      {
+        id: 2,
+        type: 'channel',
+        mainIcon: './images/done.png',
+        channelId: -1002493343663,
+        channelLink: 'https://t.me/+cU6JKcOAFuphZTli',
+        title: "Подписаться на канал",
+        description: `Название`,
+        effectIcon: "./images/coin.png",
+        effect: 10000,
+      },
+    ]
+  },
+  {
+    date: '23.09.2024',
+    tasks: [
+      {
+        id: 1,
+        type: 'friend',
+        mainIcon: 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7',
+        // mainIcon: './images/done.png',
+        title: "Пригласить друга",
+        description: 'Пригласите в игру 1 друга',
+        effectIcon: "./images/coin.png",
+        effect: 10000,
+      },
+      {
+        id: 2,
+        type: 'channel',
+        mainIcon: './images/done.png',
+        channelId: -1002493343663,
+        channelLink: 'https://t.me/+cU6JKcOAFuphZTli',
+        title: "Подписаться на канал",
+        description: `Название`,
+        effectIcon: "./images/coin.png",
+        effect: 10000,
+      },
+    ]
+  },
 ]
