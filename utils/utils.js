548a1d0a0bef964d4ba65b5e3ca3279f5924361c
Vsevolod Scherbinin <vsevolod.o.sh@yandex.ru>
2024-09-25 10:38:40 +0300

    TMA_Game_1_OOP - 0.19.8.16 Check CloudStorage Commented

--- utils/utils.js
+++ utils/utils.js
@@ -21,9 +21,10 @@ function convertStringToNumber(str) {
 }
 
 function offlineTimeCounter() {
-  // const closureDate = localStorage.getItem('closureTime');
   try {
-    const closureDate = window.Telegram.WebApp.CloudStorage.getItem('closureTime');
+  const closureDate = localStorage.getItem('closureTime');
+
+    // const closureDate = window.Telegram.WebApp.CloudStorage.getItem('closureTime');
 
   console.log('closureDate', closureDate);
   console.log(closureDate !== (null ||  undefined));
